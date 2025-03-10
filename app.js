540cb258b7d22b56d653fb0a13da037c91b11e7b
Kamogelo <ekmogasoa@gmail.com>
2025-03-11 01:01:01 +0200

    updated

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-import year from "year.js"; // Import year.js file
-
 /* This JavaScript code snippet is creating a mobile menu functionality. Here's a breakdown of what it
 does: */
 
 const mobileMenuButton = document.getElementById("mobile-menu");
 const mobileMenu = document.getElementById("mobile");
 const yearElement = document.querySelector("small");
-yearElement.textContent = `© Copyright ${year} All rights reserved  `;
+const getFullYear = new Date().getFullYear();
+yearElement.textContent = `© Copyright ${getFullYear} All rights reserved  `;
 
 const handleMobileMenu = () => {
   mobileMenu.classList.toggle("hidden"); //  Keep toggling 'hidden' for initial mobile hiding
